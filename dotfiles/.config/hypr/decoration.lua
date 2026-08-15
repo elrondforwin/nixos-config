6ae68e371d4cdf4fb746644919a412b5c1a6b341
elrondforwin <132291321+elrondforwin@users.noreply.github.com>
2026-08-15 11:39:38 +0300

    update

--- dotfiles/.config/hypr/decoration.lua
+++ dotfiles/.config/hypr/decoration.lua
@@ -1,7 +1,7 @@
 hl.config({
     general = {
         gaps_in          = 2,
-        gaps_out         = 25,
+        gaps_out         = 12,
 
         col = {
             active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
